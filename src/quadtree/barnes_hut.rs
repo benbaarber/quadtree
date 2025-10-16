80b2928eab0d0ff2804026732fbeb22f607bd357
artur <mail@xdobry.de>
2025-10-16 09:21:45 +0200

    improve same position check

--- src/quadtree/barnes_hut.rs
+++ src/quadtree/barnes_hut.rs
@@ -174,7 +174,7 @@ impl BHQuadtree {
             || split[1]-split[0] == range_len {
             // all nodes have same position, so we can not subdivide further
             // prevent endless loop
-            if self.items[split[0]].pos == self.items[split[4]-1].pos {
+            if self.items[range].windows(2).all(|w| w[0].pos == w[1].pos) {
                 self.nodes[n].children = 0;
                 return false;
             }
